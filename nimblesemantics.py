26bf8337913b43829b55259db1bf714a3a92f767
grantbrooks18 <grantbrooks18@gmail.com>
2022-02-28 15:45:26 -0500

    Setup complete

--- nimblesemantics.py
+++ nimblesemantics.py
@@ -51,3 +51,229 @@ class InferTypesAndCheckConstraints(NimbleListener):
     def __init__(self, error_log: ErrorLog):
         self.error_log = error_log
         self.current_scope = None
+
+
+# --------------------------------------------------------
+    # Program structure
+    # Deliberately left blank because the examples shown in class did not
+    # have any tests in these areas, as there is no type to assign.
+    # --------------------------------------------------------
+
+    def exitScript(self, ctx: NimbleParser.ScriptContext):
+        pass
+
+    def exitMain(self, ctx: NimbleParser.MainContext):
+        pass
+
+    def exitBody(self, ctx: NimbleParser.BodyContext):
+        pass
+
+    def exitVarBlock(self, ctx: NimbleParser.VarBlockContext):
+        pass
+
+    def exitBlock(self, ctx: NimbleParser.BlockContext):
+        pass
+
+    # --------------------------------------------------------
+    # Variable declarations
+    # --------------------------------------------------------
+
+    # def exitVarDec(self, ctx: NimbleParser.VarDecContext):
+    #     vartype = ctx.TYPE().getSymbol()
+    #     vartype = vartype.text
+    #
+    #     if vartype == "Int":
+    #         if ctx.expr():
+    #             if ctx.expr().type == PrimitiveType.Int:
+    #                 ctx.type = PrimitiveType.Int
+    #             else:
+    #                 ctx.type = PrimitiveType.ERROR
+    #                 self.error_log.add(ctx, Category.ASSIGN_TO_WRONG_TYPE,
+    #                                    f"{ctx.ID()} is declared type {vartype}\n\t"
+    #                                    f"you tried to assigning a {ctx.expr().type} to it\n\t"
+    #                                    f"This is an illegal operation. Straight to jail")
+    #
+    #         else:
+    #             ctx.type = PrimitiveType.Int
+    #
+    #     elif vartype == "Bool":
+    #
+    #         if ctx.expr():
+    #             if ctx.expr().type == PrimitiveType.Bool:
+    #                 ctx.type = PrimitiveType.Bool
+    #             else:
+    #                 ctx.type = PrimitiveType.ERROR
+    #                 self.error_log.add(ctx, Category.ASSIGN_TO_WRONG_TYPE,
+    #                                    f"{ctx.ID()} is declared type {vartype}\n\t"
+    #                                    f"you tried to assigning a {ctx.expr().type} to it\n\t"
+    #                                    f"This is an illegal operation. Straight to jail")
+    #
+    #
+    #         else:
+    #             ctx.type = PrimitiveType.Bool
+    #
+    #
+    #     elif vartype == "String":
+    #         if ctx.expr():
+    #             if ctx.expr().type == PrimitiveType.String:
+    #                 ctx.type = PrimitiveType.String
+    #             else:
+    #                 ctx.type = PrimitiveType.ERROR
+    #                 self.error_log.add(ctx, Category.ASSIGN_TO_WRONG_TYPE,
+    #                                    f"{ctx.ID()} is declared type {vartype}\n\t"
+    #                                    f"you tried to assigning a {ctx.expr().type} to it\n\t"
+    #                                    f"This is an illegal operation. Straight to jail")
+    #         else:
+    #             ctx.type = PrimitiveType.String
+    #
+    #     newkey = str(ctx.ID())
+    #
+    #     self.variables[newkey] = ctx.type
+
+        # print(self.variables)
+
+    # --------------------------------------------------------
+    # Statements
+    # --------------------------------------------------------
+
+   # def exitAssignment(self, ctx: NimbleParser.AssignmentContext):
+   #     vartype = self.variables[str(ctx.ID())]
+   #
+   #     if vartype == PrimitiveType.Int:
+   #         if ctx.expr().type == PrimitiveType.Int:
+   #             ctx.type = PrimitiveType.Int
+   #             ctx.valid = True
+   #         else:
+   #             ctx.type = PrimitiveType.ERROR
+   #             self.error_log.add(ctx, Category.ASSIGN_TO_WRONG_TYPE,
+   #                                f"{ctx.ID()} is declared type {vartype}\n\t"
+   #                                f"you tried to assigning a {ctx.expr().type} to it\n\t"
+   #                               f"This is an illegal operation. Straight to jail")
+   #
+   #
+   #     elif vartype == PrimitiveType.Bool:
+   #
+   #         if ctx.expr().type == PrimitiveType.Bool:
+   #             ctx.type = PrimitiveType.Bool
+   #             ctx.valid = True
+   #        else:
+   #             ctx.type = PrimitiveType.ERROR
+   #             ctx.valid = False
+   #             self.error_log.add(ctx, Category.ASSIGN_TO_WRONG_TYPE,
+   #                                f"{ctx.ID()} is declared type {vartype}\n\t"
+   #                                f"you tried to assigning a {ctx.expr().type} to it\n\t"
+   #                                f"This is an illegal operation. Straight to jail")
+   #     elif vartype == PrimitiveType.String:
+   #         if ctx.expr().type == PrimitiveType.String:
+   #             ctx.type = PrimitiveType.String
+   #             ctx.valid = True
+   #         else:
+   #             ctx.type = PrimitiveType.ERROR
+   #             ctx.valid = False
+   #          self.error_log.add(ctx, Category.ASSIGN_TO_WRONG_TYPE,
+   #                                f"{ctx.ID()} is declared type {vartype}\n\t"
+   #                                f"you tried to assigning a {ctx.expr().type} to it\n\t"
+   #                                f"This is an illegal operation. Straight to jail")
+   #
+   #     else:
+   #         ctx.type = PrimitiveType.ERROR
+   #         ctx.valid = False
+   #         self.error_log.add(ctx, Category.ASSIGN_TO_WRONG_TYPE,
+   #                            f"{ctx.ID()} has previously been missassigned\n\t")
+   #
+   #      newkey = str(ctx.ID())
+   #      self.variables[newkey] = ctx.type
+
+    def exitWhile(self, ctx: NimbleParser.WhileContext):
+        if ctx.expr().type != PrimitiveType.Bool:
+            ctx.type = PrimitiveType.ERROR
+            self.error_log.add(ctx, Category.CONDITION_NOT_BOOL,
+                               f"Expression {ctx.expr().text} not boolean")
+            return
+
+    def exitIf(self, ctx: NimbleParser.IfContext):
+
+        if ctx.expr().type != PrimitiveType.Bool:
+            ctx.type = PrimitiveType.ERROR
+            self.error_log.add(ctx, Category.CONDITION_NOT_BOOL,
+                               f"Expression {ctx.expr().text} not boolean")
+            return
+
+    def exitPrint(self, ctx: NimbleParser.PrintContext):
+
+        if ctx.expr().type == PrimitiveType.ERROR:
+            ctx.type = PrimitiveType.ERROR
+            self.error_log.add(ctx, Category.UNPRINTABLE_EXPRESSION,
+                               f"Can't print {ctx.op.text} ")
+
+    # --------------------------------------------------------
+    # Expressions
+    # --------------------------------------------------------
+
+    def exitIntLiteral(self, ctx: NimbleParser.IntLiteralContext):
+        ctx.type = PrimitiveType.Int
+
+    def exitNeg(self, ctx: NimbleParser.NegContext):
+
+        if ctx.op.text == '-' and ctx.expr().type == PrimitiveType.Int:
+            ctx.type = PrimitiveType.Int
+        elif ctx.op.text == '-':  # if !is used on non ints
+            ctx.type = PrimitiveType.ERROR
+            self.error_log.add(ctx, Category.INVALID_NEGATION,
+                               f"Can't apply {ctx.op.text} to {ctx.expr().type.name}")
+
+        if ctx.op.text == '!' and ctx.expr().type == PrimitiveType.Bool:
+            ctx.type = PrimitiveType.Bool
+        elif ctx.op.text == '!':  # if ! is used on non bools
+            ctx.type = PrimitiveType.ERROR
+            self.error_log.add(ctx, Category.INVALID_NEGATION,
+                               f"Can't apply {ctx.op.text} to {ctx.expr().type.name}")
+
+    def exitParens(self, ctx: NimbleParser.ParensContext):
+        ctx.type = ctx.expr().type
+
+    def exitMulDiv(self, ctx: NimbleParser.MulDivContext):
+        if ctx.expr(0).type == PrimitiveType.Int and ctx.expr(1).type == PrimitiveType.Int:
+            ctx.type = PrimitiveType.Int
+        else:
+            ctx.type = PrimitiveType.ERROR
+            self.error_log.add(ctx, Category.INVALID_BINARY_OP,
+                               f"Can't apply {ctx.op.text} to {ctx.expr(0).type.name} and {ctx.expr(1).type.name}")
+
+    def exitAddSub(self, ctx: NimbleParser.AddSubContext):
+
+        if ctx.expr(0).type == PrimitiveType.Int and ctx.expr(1).type == PrimitiveType.Int:
+            ctx.type = PrimitiveType.Int
+            return
+        elif ctx.expr(0).type == PrimitiveType.String and ctx.expr(
+                1).type == PrimitiveType.String and ctx.op.text == '+':
+            ctx.type = PrimitiveType.String
+            return
+
+        ctx.type = PrimitiveType.ERROR
+        self.error_log.add(ctx, Category.INVALID_BINARY_OP,
+                           f"Can't apply {ctx.op.text} to {ctx.expr(0).type.name} and {ctx.expr(1).type.name}")
+
+    def exitCompare(self, ctx: NimbleParser.CompareContext):
+
+        if ctx.expr(0).type == PrimitiveType.Int and ctx.expr(1).type == PrimitiveType.Int:
+            ctx.type = PrimitiveType.Bool
+        else:
+            ctx.type = PrimitiveType.ERROR
+            self.error_log.add(ctx, Category.INVALID_BINARY_OP,
+                               f"Can't apply {ctx.op.text} to {ctx.expr(0).type.name} and {ctx.expr(1).type.name}")
+
+    #def exitVariable(self, ctx: NimbleParser.VariableContext):
+     #   if str(ctx.ID()) in self.variables:
+      #      ctx.type = self.variables[str(ctx.ID())]
+#
+ #       else:
+  #          self.error_log.add(ctx,Category.UNDEFINED_NAME,
+   #                            f"This {str(ctx.ID())} has not been defined")
+
+
+    def exitStringLiteral(self, ctx: NimbleParser.StringLiteralContext):
+        ctx.type = PrimitiveType.String
+
+    def exitBoolLiteral(self, ctx: NimbleParser.BoolLiteralContext):
+        ctx.type = PrimitiveType.Bool
--- testcases.py
+++ testcases.py
@@ -14,6 +14,116 @@ from errorlog import Category
 from symboltable import PrimitiveType, FunctionType, Scope
 from testhelpers import do_semantic_analysis
 
+VALID_EXPRESSIONS = [
+    # Each entry is a pair: (expression source, expected type)
+    # Due to the way the inferred_types are stored, using ctx.getText() as the key,
+    # expressions must contain NO WHITE SPACE for the tests to work. E.g.,
+    # '59+a' is fine, '59 + a' won't work.
+    ('37', PrimitiveType.Int),
+    ('-37', PrimitiveType.Int),
+
+    ('true', PrimitiveType.Bool),
+    ('false', PrimitiveType.Bool),
+
+    ('!false', PrimitiveType.Bool),
+    ('true', PrimitiveType.Bool),
+    ('!true', PrimitiveType.Bool),
+
+    ('(37)', PrimitiveType.Int),
+    ('(-37)', PrimitiveType.Int),
+    ('(true)', PrimitiveType.Bool),
+    ('(!false)', PrimitiveType.Bool),
+
+    ('"abcdef"', PrimitiveType.String),
+    ('"Hel  l O  !"', PrimitiveType.String),
+    ('"Hel  \\a\\nl"', PrimitiveType.String),
+
+    ('1<2', PrimitiveType.Bool),
+    ('(1==2)', PrimitiveType.Bool),
+    ('(1==-2)', PrimitiveType.Bool),
+
+    ('1+2', PrimitiveType.Int),
+    ('(1--2)', PrimitiveType.Int),
+    ('"HELLO"+"WORLD"', PrimitiveType.String),
+
+    ('1*3', PrimitiveType.Int),
+    ('(1/2*4)', PrimitiveType.Int),
+    ('((1*2)/4*-2)', PrimitiveType.Int),
+
+]
+
+INVALID_EXPRESSIONS = [
+    # Each entry is a pair: (expression source, expected error category)
+    # As for VALID_EXPRESSIONS, there should be NO WHITE SPACE in the expressions.
+    ('!37', Category.INVALID_NEGATION),
+    ('!!37', Category.INVALID_NEGATION),
+
+    ('-true', Category.INVALID_NEGATION),
+    ('-false', Category.INVALID_NEGATION),
+
+    ('true<3', Category.INVALID_BINARY_OP),
+    ('true<!false', Category.INVALID_BINARY_OP),
+    ('!true==false', Category.INVALID_BINARY_OP),
+
+    ('"HELLO"-"WORLD"', Category.INVALID_BINARY_OP),
+    ('true+false', Category.INVALID_BINARY_OP),
+    ('true-4', Category.INVALID_BINARY_OP),
+    ('"abc"+false', Category.INVALID_BINARY_OP),
+
+    ('"HELLO"-"WORLD"', Category.INVALID_BINARY_OP),
+    ('true+false', Category.INVALID_BINARY_OP),
+    ('true-4', Category.INVALID_BINARY_OP),
+    ('"abc"+false', Category.INVALID_BINARY_OP),
+
+    ('"HELLO"/"WORLD"', Category.INVALID_BINARY_OP),
+    ('true*false', Category.INVALID_BINARY_OP),
+    ('true*4', Category.INVALID_BINARY_OP),
+    ('"abc"/false', Category.INVALID_BINARY_OP),
+
+]
+
+VARDEC_TESTS = [
+
+    ("var Apple : Int", PrimitiveType.Int, "Apple"),
+    ("var Apple : Int = 1 ", PrimitiveType.Int, "Apple"),
+    ("var Apple : Int = true ", PrimitiveType.ERROR, "Apple"),
+    ("var Apple : Int = \"Hello\" ", PrimitiveType.ERROR, "Apple"),
+
+
+    ("var Pear : Bool = true ", PrimitiveType.Bool, "Pear"),
+    ("var Pear : Bool = false ", PrimitiveType.Bool, "Pear"),
+    ("var Pear : Bool = \"Hello\" ", PrimitiveType.ERROR, "Pear"),
+    ("var Pear : Bool = 654 ", PrimitiveType.ERROR, "Pear"),
+    ("var Pear : Bool", PrimitiveType.Bool, "Pear"),
+
+
+    ("var nectarine : String = 654 ", PrimitiveType.ERROR, "nectarine"),
+    ("var nectarine : String = \"Hello\" ", PrimitiveType.String, "nectarine"),
+    ("var nectarine : String = true ", PrimitiveType.ERROR, "nectarine"),
+    ("var nectarine : String", PrimitiveType.String, "nectarine"),
+]
+
+VARASSIGN_TESTS_valid = [
+    ("Apple = 1", PrimitiveType.Int, "Apple",{"Apple" : PrimitiveType.Int} ),
+    ("Pear = true", PrimitiveType.Bool,"Pear", {"Pear" : PrimitiveType.Bool}),
+    ("nectarine = \"The Best Fruit\"", PrimitiveType.String,"nectarine", {"nectarine": PrimitiveType.String }),
+]
+
+VARASSIGN_TESTS_invalid = [
+    ("Apple = true", PrimitiveType.ERROR,  "Apple",{"Apple" : PrimitiveType.Int}),
+    ("Apple = \"Hello\"", PrimitiveType.ERROR,  "Apple",{"Apple" : PrimitiveType.Int}),
+    ("Apple = 1", PrimitiveType.ERROR,  "Apple",{"Apple" : PrimitiveType.ERROR}),
+
+    ("Pear = 5", PrimitiveType.ERROR, "Pear",{"Pear" : PrimitiveType.Bool}),
+    ("Pear = \"Hello\"", PrimitiveType.ERROR, "Pear",{"Pear" : PrimitiveType.Bool}),
+    ("Pear = true", PrimitiveType.ERROR, "Pear",{"Pear" : PrimitiveType.ERROR}),
+
+    ("nectarine = 5", PrimitiveType.ERROR, "nectarine", {"nectarine": PrimitiveType.String }),
+    ("nectarine = true", PrimitiveType.ERROR, "nectarine", {"nectarine": PrimitiveType.String }),
+    ("nectarine = \"Hello\"", PrimitiveType.ERROR, "nectarine", {"nectarine": PrimitiveType.ERROR }),
+
+]
+
 
 class TypeAndStatementTests(unittest.TestCase):
     """
@@ -24,7 +134,131 @@ class TypeAndStatementTests(unittest.TestCase):
     You should be able to re-use your tests from lab 3 here, adjusting for the new signature
     of `do_semantic_analysis`. All tests should succeed, except those using variables.
     """
-    pass
+
+
+    VARDEC_TESTS = [
+
+        ("var Apple : Int", PrimitiveType.Int, "Apple"),
+        ("var Apple : Int = 1 ", PrimitiveType.Int, "Apple"),
+        ("var Apple : Int = true ", PrimitiveType.ERROR, "Apple"),
+        ("var Apple : Int = \"Hello\" ", PrimitiveType.ERROR, "Apple"),
+
+        ("var Pear : Bool = true ", PrimitiveType.Bool, "Pear"),
+        ("var Pear : Bool = false ", PrimitiveType.Bool, "Pear"),
+        ("var Pear : Bool = \"Hello\" ", PrimitiveType.ERROR, "Pear"),
+        ("var Pear : Bool = 654 ", PrimitiveType.ERROR, "Pear"),
+        ("var Pear : Bool", PrimitiveType.Bool, "Pear"),
+
+        ("var nectarine : String = 654 ", PrimitiveType.ERROR, "nectarine"),
+        ("var nectarine : String = \"Hello\" ", PrimitiveType.String, "nectarine"),
+        ("var nectarine : String = true ", PrimitiveType.ERROR, "nectarine"),
+        ("var nectarine : String", PrimitiveType.String, "nectarine"),
+    ]
+
+    VARASSIGN_TESTS_valid = [
+        ("Apple = 1", PrimitiveType.Int, "Apple", {"Apple": PrimitiveType.Int}),
+        ("Pear = true", PrimitiveType.Bool, "Pear", {"Pear": PrimitiveType.Bool}),
+        ("nectarine = \"The Best Fruit\"", PrimitiveType.String, "nectarine", {"nectarine": PrimitiveType.String}),
+    ]
+
+    VARASSIGN_TESTS_invalid = [
+        ("Apple = true", PrimitiveType.ERROR, "Apple", {"Apple": PrimitiveType.Int}),
+        ("Apple = \"Hello\"", PrimitiveType.ERROR, "Apple", {"Apple": PrimitiveType.Int}),
+        ("Apple = 1", PrimitiveType.ERROR, "Apple", {"Apple": PrimitiveType.ERROR}),
+
+        ("Pear = 5", PrimitiveType.ERROR, "Pear", {"Pear": PrimitiveType.Bool}),
+        ("Pear = \"Hello\"", PrimitiveType.ERROR, "Pear", {"Pear": PrimitiveType.Bool}),
+        ("Pear = true", PrimitiveType.ERROR, "Pear", {"Pear": PrimitiveType.ERROR}),
+
+        ("nectarine = 5", PrimitiveType.ERROR, "nectarine", {"nectarine": PrimitiveType.String}),
+        ("nectarine = true", PrimitiveType.ERROR, "nectarine", {"nectarine": PrimitiveType.String}),
+        ("nectarine = \"Hello\"", PrimitiveType.ERROR, "nectarine", {"nectarine": PrimitiveType.ERROR}),
+
+    ]
+
+    def test_valid_expressions(self):
+        """
+        For each pair (expression source, expected type) in VALID_EXPRESSIONS, verifies
+        that the expression's inferred type is as expected, and that there are no errors
+        in the error log.
+        """
+        for expression, expected_type in VALID_EXPRESSIONS:
+            log, global_scope, inferred_types = do_semantic_analysis(expression, 'expr')
+            # if expression == '-37':
+            #     print_debug_info(expression, inferred_types, log)
+            with self.subTest(expression=expression, expected_type=expected_type):
+                self.assertEqual(expected_type, inferred_types[1][expression])
+                self.assertEqual(0, log.total_entries())
+
+    def test_invalid_expressions(self):
+        """
+        For each pair (expression source, expected error category) in INVALID_EXPRESSIONS,
+        verifies that the expression is assigned the ERROR type and that there is a logged
+        error of the expected category relating to the expression.
+        """
+        for expression, expected_category in INVALID_EXPRESSIONS:
+            log, global_scope, inferred_types = do_semantic_analysis(expression, 'expr')
+            # if expression == '!!37':
+            #     print_debug_info(expression, inferred_types, log)
+            with self.subTest(expression=expression,
+                              expected_category=expected_category):
+                self.assertEqual(PrimitiveType.ERROR, inferred_types[1][expression])
+                self.assertTrue(log.includes_exactly(expected_category, 1, expression))
+
+    # def test_variable_Declaration(self):
+    #     """
+    #     This was stolen from the above examples to rapidly loop through variables and see if they match their types.
+    #
+    #     This testcase uses a third field that is just the ID to allow for lookup of the
+    #     variable in the variable dictionary
+    #     """
+    #
+    #     for expression, expected_type, ID in VARDEC_TESTS:
+    #         log, global_scope, inferred_types = do_semantic_analysis(expression, 'varDec')
+    #         # if expression == '-37':
+    #         #     print_debug_info(expression, inferred_types, log)
+    #         with self.subTest(expression=expression, expected_type=expected_type):
+    #             self.assertEqual(expected_type, variables[ID])
+    #
+    # def test_variable_assignment(self):
+    #     """
+    #     This was stolen from the above examples to rapidly loop through variables and see if they match their types.
+    #
+    #     This testcase uses a third field that is just the ID to allow for lookup of the
+    #     variable in the variable dictionary
+    #     """
+    #
+    #     for expression, expected_type, ID, setup in VARASSIGN_TESTS_valid:
+    #         log, global_scope, inferred_types = do_semantic_analysis_initial_condition(expression, 'statement', setup)
+    #         with self.subTest(expression=expression, expected_type=expected_type):
+    #             self.assertEqual(expected_type, variables[ID])
+    #             self.assertEqual(0, log.total_entries())
+    #
+    #     for expression, expected_type, ID, setup in VARASSIGN_TESTS_invalid:
+    #         log, global_scope, inferred_types = do_semantic_analysis_initial_condition(expression, 'statement', setup)
+    #         with self.subTest(expression=expression, expected_type=expected_type):
+    #             self.assertEqual(expected_type, variables[ID])
+    #             self.assertEqual(1, log.total_entries())
+
+    def test_print_primitive(self):
+        log, global_scope, inferred_types = do_semantic_analysis("print 123", 'main')
+        self.assertEqual(0, log.total_entries())
+
+        log, global_scope, inferred_types = do_semantic_analysis('print "hello"', 'main')
+        self.assertEqual(0, log.total_entries())
+
+        log, global_scope, inferred_types = do_semantic_analysis("print true", 'main')
+        self.assertEqual(0, log.total_entries())
+
+    def test_if_while_primitive(self):
+        log, global_scope, inferred_types = do_semantic_analysis("if true { }", 'main')
+        self.assertEqual(0, log.total_entries())
+
+        log, global_scope, inferred_types = do_semantic_analysis('if true { } else { }', 'main')
+        self.assertEqual(0, log.total_entries())
+
+        log, global_scope, inferred_types = do_semantic_analysis("while true { }", 'main')
+        self.assertEqual(0, log.total_entries())
 
 
 class ScopeCreationTests(unittest.TestCase):
